d11bce305a7a0776eef8f6b6d0f2173eb3f9327f
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 12:44:34 -0600

    Explore integer precision and limits

--- numbersInCS/Program.cs
+++ numbersInCS/Program.cs
@@ -34,8 +34,19 @@ namespace numbersInCS
             int n = 4;
             int o = 3;
             int p = (m + n) / o;
+            int q = (m + n) % o;
 
-            Console.WriteLine(p);
+            //Console.WriteLine(p);
+            //Console.WriteLine(q);
+            Console.WriteLine($"qoutient: {p}");
+            Console.WriteLine($"remainder: {q}");
+
+            int max = int.MaxValue;
+            int min = int.MinValue;
+            Console.WriteLine($"The range of integers is {min} to {max}.");
+
+            int what = max + 3;
+            Console.WriteLine($"An example of overflow: {what}");
         }
     }
 }

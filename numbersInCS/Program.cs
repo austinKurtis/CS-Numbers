028d8563f128504ca92113adaeb11f61d787ce04
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 12:15:55 -0600

    Explore int Math

--- numbersInCS/Program.cs
+++ numbersInCS/Program.cs
@@ -6,7 +6,17 @@ namespace numbersInCS
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int a = 18;
+            int b = 6;
+            int c = a + b;
+            int d = a - b;
+            int e = a * b;
+            int f = a / b;
+
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
         }
     }
 }

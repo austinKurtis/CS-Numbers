5b7a3e83b65edb38971f99806f7737a113f9e55c
AustinKurtis <austin.jay.kurtis@gmail.com>
2018-01-31 12:32:37 -0600

    Explore Order of Operations

--- numbersInCS/Program.cs
+++ numbersInCS/Program.cs
@@ -17,6 +17,25 @@ namespace numbersInCS
             Console.WriteLine(d);
             Console.WriteLine(e);
             Console.WriteLine(f);
+
+            int g = 5;
+            int h = 4;
+            int i = 2;
+            int j = g + h * i;
+            int k = (g + h) * i;
+
+            Console.WriteLine(j);
+            Console.WriteLine(k);
+
+            int l = (g + h) - 6 * i + (12 * 4) / 3 + 12;
+            Console.WriteLine(l);
+
+            int m = 7;
+            int n = 4;
+            int o = 3;
+            int p = (m + n) / o;
+
+            Console.WriteLine(p);
         }
     }
 }
